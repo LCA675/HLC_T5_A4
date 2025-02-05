3bc95ff4689a8269032e8eb60936f2ace494e85e
LCA675 <lauracalleanguita@gmail.com>
2025-02-05 14:38:01 +0100

    Ejercciio 5 sin terminar

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,6 +1,21 @@
+# n=int(input("Introduce una lista de números: "))
+# def contar_digitos(n):
+#     return len(str(n+1))
+
+# resultado=contar_digitos(n)
+# print(resultado)
+
 n=int(input("Introduce una lista de números: "))
 def contar_digitos(n):
-    return len(str(n+1))
+    resto=0
+    contador=0
+    if n==0:
+        return 0
+    else:
+        resto=n/10
+        contador+=contador
+    return contador
+
 
 resultado=contar_digitos(n)
 print(resultado)
