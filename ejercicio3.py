de3e16e447844c962e315d807934cf8090047953
LCA675 <lauracalleanguita@gmail.com>
2025-02-05 13:08:24 +0100

    Ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,11 @@
+def potencia(a, b):
+    if b==0:
+        return 1
+    elif b==1:
+        return a
+    return a* potencia(a, b-1)
+
+a=int(input("Introduce un número base: "))
+b=int(input("Introduce un exponente: "))
+resultado=potencia(a, b)
+print(resultado)
